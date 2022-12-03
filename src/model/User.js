fb70e581293ae20a228b9748c55aa001f53e3529
Sid <siddharthsathe@yahoo.com>
2022-12-03 13:42:26 +0530

    chore: save timestamps

--- src/model/User.js
+++ src/model/User.js
@@ -1,13 +1,18 @@
 const dynamoose = require("dynamoose");
 const User = dynamoose.model(
   "User",
-  new dynamoose.Schema({
-    id: String, // first field of schema is hash key
-    firstName: String,
-    lastName: String,
-    email: String,
-    country: String,
-  })
+  new dynamoose.Schema(
+    {
+      id: String, // first field of schema is hash (primary) key
+      firstName: String,
+      lastName: String,
+      email: String,
+      country: String,
+    },
+    {
+      timestamps: true,
+    }
+  )
 );
 
 module.exports.User = User;
